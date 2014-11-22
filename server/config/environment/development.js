ee42bbe425f6ff43f7d420c28b04c95ac229beaa
Mary Le <maaaryle@gmail.com>
2014-11-22 11:25:44 -0800

    generator init

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/weebly2-dev'
+  },
+
+  seedDB: true
+};
